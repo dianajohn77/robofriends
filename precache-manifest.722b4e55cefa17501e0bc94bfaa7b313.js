ed9cde48cee67ab6b6764fd2e225f8706c1642bb
Diana <diana@Dianas-MacBook-Air.local>
2020-05-17 08:16:37 +0530

    Updates

---
+++ precache-manifest.722b4e55cefa17501e0bc94bfaa7b313.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cc817f390883291388eff5e87f06a3b",
+    "url": "/dianajohn77/robofriends.git/index.html"
+  },
+  {
+    "revision": "11344871630b1a0c4681",
+    "url": "/dianajohn77/robofriends.git/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "4e8043069be7556c3a74",
+    "url": "/dianajohn77/robofriends.git/static/css/main.629fbb19.chunk.css"
+  },
+  {
+    "revision": "11344871630b1a0c4681",
+    "url": "/dianajohn77/robofriends.git/static/js/2.800020c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dianajohn77/robofriends.git/static/js/2.800020c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e8043069be7556c3a74",
+    "url": "/dianajohn77/robofriends.git/static/js/main.546807c9.chunk.js"
+  },
+  {
+    "revision": "361303d7d8664b629108",
+    "url": "/dianajohn77/robofriends.git/static/js/runtime-main.94f64b0b.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/dianajohn77/robofriends.git/static/media/SEGA.12a3db8b.woff"
+  }
+]);
